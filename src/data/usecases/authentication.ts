f40a891e9cabd8e0d6a6844ffc2d4f1b45172eee
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-26 14:58:44 -0300

    feat: apply exception unathorized

--- src/data/usecases/authentication.ts
+++ src/data/usecases/authentication.ts
@@ -1,3 +1,5 @@
+import { HttpStatusCode } from '../../@types/http.response'
+import { InvalidCredentialsError } from '../../core/exceptions/invalid.credentials.error'
 import { type IHttpPostClient } from '../protocols/http/http.post.client.protocol'
 import { type authParamns } from '../protocols/usecases/authentication.protocol'
 
@@ -8,9 +10,13 @@ export class Authentication {
   ) {}
 
   async auth (params: authParamns): Promise<void> {
-    await this.HttpPostClient.post({
+    const httpResponse = await this.HttpPostClient.post({
       url: this.url,
       body: params
     })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unathorized :
+        throw new InvalidCredentialsError()
+    }
   }
 }
