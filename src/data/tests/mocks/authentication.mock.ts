308ce64c290ed3b80951b789e43c47e2d783f4c5
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-26 15:37:05 -0300

    feat: ensure return account if sucess

--- src/data/tests/mocks/authentication.mock.ts
+++ src/data/tests/mocks/authentication.mock.ts
@@ -1,7 +1,12 @@
 import { faker } from '@faker-js/faker'
 import { type authParamns } from '../../protocols/usecases/authentication.protocol'
+import { type accountProps } from '../../../domain/entities/account'
 
 export const authMock = (): authParamns => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const accountMock = (): accountProps => ({
+  accessToken: faker.string.uuid()
 })
