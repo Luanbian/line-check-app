797465afa04a35ef0fcba33891334f9b64f2e503
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-26 14:22:12 -0300

    refactor: use faker instead strings

--- src/data/tests/authentication.spec.ts
+++ src/data/tests/authentication.spec.ts
@@ -1,13 +1,14 @@
 import { describe, expect, test } from 'vitest'
 import { Authentication } from '../usecases/authentication'
 import { HttpPostClientMock } from './mocks/http.post.client.mock'
+import { faker } from '@faker-js/faker'
 
 interface SutTypes {
   sut: Authentication
   httpPostClientMock: HttpPostClientMock
 }
 
-const makeSut = (url: string = 'any_string'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientMock = new HttpPostClientMock()
   const sut = new Authentication(url, httpPostClientMock)
   return {
@@ -18,7 +19,7 @@ const makeSut = (url: string = 'any_string'): SutTypes => {
 
 describe('Authentication', () => {
   test('should call HttpPostClient with correct URL', async () => {
-    const url = 'fake_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientMock } = makeSut(url)
     await sut.auth()
     expect(httpPostClientMock.url).toBe(url)
