7b1196765fd8162e6b930564895e7ac7c4ff3f72
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-26 15:36:48 -0300

    feat: return account if sucess

--- src/data/usecases/authentication.ts
+++ src/data/usecases/authentication.ts
@@ -1,6 +1,7 @@
 import { HttpStatusCode } from '../../@types/http.response'
 import { InvalidCredentialsError } from '../../core/exceptions/invalid.credentials.error'
 import { UnexpectedError } from '../../core/exceptions/unexpected.error'
+import { type accountProps } from '../../domain/entities/account'
 import { type IHttpPostClient } from '../protocols/http/http.post.client.protocol'
 import { type authParamns } from '../protocols/usecases/authentication.protocol'
 
@@ -10,13 +11,13 @@ export class Authentication {
     private readonly HttpPostClient: IHttpPostClient
   ) {}
 
-  async auth (params: authParamns): Promise<void> {
+  async auth (params: authParamns): Promise<accountProps> {
     const httpResponse = await this.HttpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body as accountProps
       case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
