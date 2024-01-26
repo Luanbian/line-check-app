3d14939a43e61920d0d9df825fc94a7812019106
Luan Bianchini <luanbianchine1@gmail.com>
2024-01-26 14:48:02 -0300

    feat: add httpResponse

--- src/data/protocols/http/http.post.client.protocol.ts
+++ src/data/protocols/http/http.post.client.protocol.ts
@@ -1,8 +1,10 @@
+import { type HttpResponse } from '../../../@types/http.response'
+
 export interface httpPostParams {
   url: string
   body?: object
 }
 
 export interface IHttpPostClient {
-  post: (params: httpPostParams) => Promise<void>
+  post: (params: httpPostParams) => Promise<HttpResponse>
 }
--- src/data/tests/mocks/http.post.client.mock.ts
+++ src/data/tests/mocks/http.post.client.mock.ts
@@ -1,12 +1,17 @@
+import { HttpStatusCode, type HttpResponse } from '../../../@types/http.response'
 import { type httpPostParams, type IHttpPostClient } from '../../protocols/http/http.post.client.protocol'
 
 export class HttpPostClientMock implements IHttpPostClient {
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.unathorized,
+    body: null
+  }
 
-  async post (params: httpPostParams): Promise<void> {
+  async post (params: httpPostParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
-    await Promise.resolve()
+    return await Promise.resolve(this.response)
   }
 }
