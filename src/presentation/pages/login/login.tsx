09d1a9c64c7bd52c60b7fd08284bee0fa4abca36
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-02 13:33:32 -0300

    feat: init login screen

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function Login (): React.JSX.Element {
+  return (
+    <View>
+      <Text>Login page</Text>
+    </View>
+  )
+}
