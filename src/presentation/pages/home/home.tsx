cf7218c258e4156d16dae3f60b8f433836b35c3b
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-02 18:29:18 -0300

    feat: init home page

---
+++ src/presentation/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Home (): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hi!, this is home page</Text>
+    </View>
+  )
+}
