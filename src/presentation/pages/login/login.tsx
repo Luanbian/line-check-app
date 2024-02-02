8398585f2651e8f6f8ab2603684b8219af9f0c61
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-02 14:22:12 -0300

    feat: create login screen and submit inputs

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { Text, View, TextInput, Button } from 'react-native'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 export default function Login (): React.JSX.Element {
+  const { register, setValue, handleSubmit } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => { console.log(data.email, data.password) }
+
+  useEffect(() => {
+    register('email')
+    register('password')
+  }, [register])
+
   return (
     <View>
       <Text>Login page</Text>
+      <TextInput placeholder='email' onChangeText={text => { setValue('email', text) }}/>
+      <TextInput placeholder='senha' onChangeText={text => { setValue('password', text) }} secureTextEntry />
+      <Button title='Enviar' onPress={handleSubmit(onSubmit)}/>
     </View>
   )
 }
