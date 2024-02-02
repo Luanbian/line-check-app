3204bbeefbfc867877e07ca4e8f94f41fdd2cbd3
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-02 14:43:08 -0300

    feat: implemetns validation field

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react'
 import { Text, View, TextInput, Button } from 'react-native'
 import { useForm, type SubmitHandler } from 'react-hook-form'
+import { object, string } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const fieldsValidationSchema = object({
+  email: string().email('email inválido').required('O email é obrigatório'),
+  password: string().required('A senha é obrigatória')
+})
 
 interface Inputs {
   email: string
@@ -8,8 +15,12 @@ interface Inputs {
 }
 
 export default function Login (): React.JSX.Element {
-  const { register, setValue, handleSubmit } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => { console.log(data.email, data.password) }
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<Inputs>({
+    resolver: yupResolver(fieldsValidationSchema)
+  })
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data.email, data.password)
+  }
 
   useEffect(() => {
     register('email')
@@ -20,7 +31,9 @@ export default function Login (): React.JSX.Element {
     <View>
       <Text>Login page</Text>
       <TextInput placeholder='email' onChangeText={text => { setValue('email', text) }}/>
+      {(errors.email != null) && <Text>{errors.email.message}</Text>}
       <TextInput placeholder='senha' onChangeText={text => { setValue('password', text) }} secureTextEntry />
+      {(errors.password != null) && <Text>{errors.password.message}</Text>}
       <Button title='Enviar' onPress={handleSubmit(onSubmit)}/>
     </View>
   )
