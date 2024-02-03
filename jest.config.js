886dda3201525c1b069a2b6d240d64f26307f9a1
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-03 16:09:51 -0300

    chore: add tsx in coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.spec.ts', '**/*.spec.tsx'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: 'jest-expo',
