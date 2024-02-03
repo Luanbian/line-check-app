becf7ffd7a728d446229750b960263007d24588f
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-03 16:23:01 -0300

    refactor: add sut and cleanup in login spec

--- src/presentation/pages/tests/login.spec.tsx
+++ src/presentation/pages/tests/login.spec.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
+import { type RenderResult, render, cleanup } from '@testing-library/react-native'
 import Login from '../login/login'
+
+interface SutTypes {
+  sut: RenderResult
+}
 
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: () => ({ navigate: jest.fn() })
 }))
 
+const makeSut = (): SutTypes => {
+  const sut = render(<Login/>)
+  return {
+    sut
+  }
+}
+
 describe('login page', () => {
+  afterEach(cleanup)
   test('initial state', async () => {
-    const { getByTestId, queryByTestId } = render(<Login/>)
-    const title = getByTestId('title')
-    const email = getByTestId('emailField')
-    const password = getByTestId('passwordField')
-    const submit = getByTestId('submitButton')
-    const errorEmail = queryByTestId('error-email')
-    const errorPassword = queryByTestId('error-password')
+    const { sut } = makeSut()
+    const title = sut.getByTestId('title')
+    const email = sut.getByTestId('emailField')
+    const password = sut.getByTestId('passwordField')
+    const submit = sut.getByTestId('submitButton')
+    const errorEmail = sut.queryByTestId('error-email')
+    const errorPassword = sut.queryByTestId('error-password')
     expect(title.props.children).toBe('Login page')
     expect(email).toBeDefined()
     expect(password).toBeDefined()
