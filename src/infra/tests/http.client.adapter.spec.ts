1efdba790ae4a8baa49c4ef01afb42aea1199002
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-04 17:44:17 -0300

    test: catch part of http client adapter

--- src/infra/tests/http.client.adapter.spec.ts
+++ src/infra/tests/http.client.adapter.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientAdapter } from '../adapters/http.client.adapter'
+import { type AxiosError } from 'axios'
 import type axios from 'axios'
 import { mockAxios, mockPostRequest } from './mocks/axios.mock'
 
@@ -29,5 +30,38 @@ describe('HttpClientAdapter', () => {
     const { sut, mockedAxios } = makeSut()
     const promise = sut.post(mockPostRequest())
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  })
+  test('should return http error if axios return own error', async () => {
+    const { sut, mockedAxios } = makeSut()
+    const mockedError: AxiosError = {
+      isAxiosError: true,
+      message: 'Some error message',
+      name: 'Axios error',
+      toJSON: {} as any,
+      response: {
+        config: {
+          headers: {} as any
+        },
+        data: 'Error data',
+        headers: {} as any,
+        status: 400,
+        statusText: 'Erro message text'
+      }
+    }
+    mockedAxios.post.mockRejectedValueOnce(mockedError)
+    try {
+      await sut.post(mockPostRequest())
+    } catch (error) {
+      expect((error as AxiosError).isAxiosError).toBe(true)
+      expect((error as AxiosError).response).toEqual({
+        config: {
+          headers: {} as any
+        },
+        data: 'Error data',
+        headers: {} as any,
+        status: 400,
+        statusText: 'Erro message text'
+      })
+    }
   })
 })
