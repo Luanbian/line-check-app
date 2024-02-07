6fac274e81c03351c07cd3893d09c4f15586e37b
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-07 16:16:39 -0300

    feat: add manager component in router

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../../pages/home/home'
 import { makeLogin } from '../../../core/factories/pages/login/login.factory'
+import Manager from '../../pages/manager/manager'
 
 const Stack = createNativeStackNavigator()
 
@@ -16,6 +17,10 @@ export default function Router (): React.JSX.Element {
         <Stack.Screen
           name="Home"
           component={Home}
+        />
+        <Stack.Screen
+          name="Manager"
+          component={Manager}
         />
       </Stack.Navigator>
     </NavigationContainer>
