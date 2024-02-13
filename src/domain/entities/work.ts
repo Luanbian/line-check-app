a34a8d48bf1b645a434c2de9d1fe42d7bb21120c
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-13 16:10:34 -0300

    feat: add entity work

---
+++ src/domain/entities/work.ts
@@ -0,0 +1,21 @@
+export interface workProps {
+  id: string
+  driver: string
+  startJourneyModel: string
+  startLineModel: string
+  endExpedientModel: string
+  startJourneyReal: string
+  startLineReal: string
+  endExpedientReal: string
+  service: string
+  road: string
+  manufacture: string
+  vehicle: string
+  monday: boolean
+  tuesday: boolean
+  wednesday: boolean
+  thursday: boolean
+  friday: boolean
+  saturday: boolean
+  sunday: boolean
+}
