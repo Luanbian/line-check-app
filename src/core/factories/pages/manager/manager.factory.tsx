4d1c680261f7b2d91565dfca5ca092d0a6727b87
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-16 16:58:46 -0300

    feat: manager screen show what need

--- src/core/factories/pages/manager/manager.factory.tsx
+++ src/core/factories/pages/manager/manager.factory.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Manager from '../../../../presentation/pages/manager/manager'
+import { makeLocalStorage } from '../../infra/adapters/local.storage.factory'
+import { makeWorkInfoComplete } from '../../data/work/work.info.factory'
 
 export const makeManager: React.FC = () => {
+  const localStorage = makeLocalStorage()
+  const workInfoComplete = makeWorkInfoComplete()
   return (
-    <Manager />
+    <Manager
+      localStorage={ localStorage }
+      workInfoComplete={ workInfoComplete }
+    />
   )
 }
--- src/presentation/pages/manager/manager.tsx
+++ src/presentation/pages/manager/manager.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Button } from 'react-native'
+import { type ILocalStorage } from '../../../infra/protocols/local.storage.protocol'
+import { type IWorkInfoComplete } from '../../../data/protocols/usecases/work.info.protocol'
+import { type workPropsComplete } from '../../../domain/entities/work'
 
-export default function Manager (): React.JSX.Element {
+interface Props {
+  localStorage: ILocalStorage
+  workInfoComplete: IWorkInfoComplete
+}
+
+export default function Manager ({ localStorage, workInfoComplete }: Props): React.JSX.Element {
+  const [data, setData] = useState<workPropsComplete[]>()
+
+  const getTest = async (): Promise<void> => {
+    const token = await localStorage.obtain('token')
+    if (token != null) {
+      const httpRes = await workInfoComplete.perform(token)
+      setData(httpRes[0])
+    }
+  }
   return (
     <View>
       <Text>Hi!, this is manager page</Text>
+      <Button title='Teste' onPress={async () => { await getTest() }} />
+      {data?.map(item => (
+        <View key={item.id}>
+          <Text>Driver: {item.accountName}</Text>
+          <Text>Service: {item.service}</Text>
+          <Text>inicio jornada: {item.startJourneyModel}</Text>
+          <Text>inicio jornada real: {item.startJourneyReal}</Text>
+          <Text>fim jornada real: {item.endLineReal}</Text>
+        </View>
+      ))}
     </View>
   )
 }
