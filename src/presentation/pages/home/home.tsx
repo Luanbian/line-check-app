5e9e44856c95127423ba507985adf8e767f6537b
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-16 16:59:10 -0300

    feat: implement error message in screen

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -13,6 +13,7 @@ interface Props {
 
 export default function Home ({ getWorkInfo, localStorage, updateLinecheck }: Props): React.JSX.Element {
   const [data, setData] = useState<workProps[]>()
+  const [errorSubmit, setErrorSubmit] = useState<string | null>(null)
 
   const getTest = async (): Promise<void> => {
     const token = await localStorage.obtain('token')
@@ -23,21 +24,28 @@ export default function Home ({ getWorkInfo, localStorage, updateLinecheck }: Pr
   }
 
   const putTest = async (workId: string, marker: LinecheckOptions): Promise<void> => {
-    const token = await localStorage.obtain('token')
-    const accountId = await localStorage.obtain('accountId')
-    if (token != null && accountId != null) {
-      await updateLinecheck.perform({
-        workId,
-        accountId,
-        marker,
-        token
-      })
+    try {
+      const token = await localStorage.obtain('token')
+      const accountId = await localStorage.obtain('accountId')
+      if (token != null && accountId != null) {
+        await updateLinecheck.perform({
+          workId,
+          accountId,
+          marker,
+          token
+        })
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorSubmit(error.message)
+      }
     }
   }
 
   return (
     <View>
       <Text>Hi!, this is home page</Text>
+      {(errorSubmit != null) && <Text>{errorSubmit}</Text>}
       <Button title='Teste' onPress={async () => { await getTest() }} />
       {data?.map(item => (
         <View key={item.id}>
