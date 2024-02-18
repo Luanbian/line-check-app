7d6a43ee121128b83961221b9b714aa2f2da48c5
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-17 21:29:09 -0300

    refactor: Home page

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, Button, ScrollView } from 'react-native'
 import { type IWorkInfo } from '../../../data/protocols/usecases/work.info.protocol'
 import { type ILocalStorage } from '../../../infra/protocols/local.storage.protocol'
 import { type workProps } from '../../../domain/entities/work'
@@ -15,18 +15,23 @@ export default function Home ({ getWorkInfo, localStorage, updateLinecheck }: Pr
   const [data, setData] = useState<workProps[]>()
   const [errorSubmit, setErrorSubmit] = useState<string | null>(null)
 
-  const getTest = async (): Promise<void> => {
-    const token = await localStorage.obtain('token')
-    if (token != null) {
-      const httpRes = await getWorkInfo.perform(token)
-      setData(httpRes[0])
-    }
-  }
-
-  const putTest = async (workId: string, marker: LinecheckOptions): Promise<void> => {
-    try {
+  useEffect(() => {
+    const getWorkDriverInfo = async (): Promise<void> => {
       const token = await localStorage.obtain('token')
-      const accountId = await localStorage.obtain('accountId')
+      if (token != null) {
+        const httpRes = await getWorkInfo.perform(token)
+        setData(httpRes[0])
+      }
+    }
+    void getWorkDriverInfo()
+  }, [])
+
+  const updateDriverLinecheck = async (workId: string, marker: LinecheckOptions): Promise<void> => {
+    try {
+      const [token, accountId] = await Promise.all([
+        localStorage.obtain('token'),
+        localStorage.obtain('accountId')
+      ])
       if (token != null && accountId != null) {
         await updateLinecheck.perform({
           workId,
@@ -43,20 +48,23 @@ export default function Home ({ getWorkInfo, localStorage, updateLinecheck }: Pr
   }
 
   return (
-    <View>
-      <Text>Hi!, this is home page</Text>
+    <ScrollView>
       {(errorSubmit != null) && <Text>{errorSubmit}</Text>}
-      <Button title='Teste' onPress={async () => { await getTest() }} />
       {data?.map(item => (
-        <View key={item.id}>
-          <Text>Driver: {item.accountName}</Text>
-          <Text>Service: {item.service}</Text>
+        <View key={item.id} style={{ borderColor: 'red', borderWidth: 5 }}>
+          <Text>Motorista: {item.accountName}</Text>
           <Text>inicio jornada: {item.startJourneyModel}</Text>
-          <Button title='Check start journey' onPress={async () => { await putTest(item.id, 'STARTJOURNEYREAL') }} />
-          <Button title='Check start line' onPress={async () => { await putTest(item.id, 'STARTLINEREAL') }} />
-          <Button title='Check end journey' onPress={async () => { await putTest(item.id, 'ENDLINEREAL') }} />
+          <Button title='Check start journey' onPress={async () => { await updateDriverLinecheck(item.id, 'STARTJOURNEYREAL') }} />
+          <Text>inicio linha: {item.startLineModel}</Text>
+          <Button title='Check start line' onPress={async () => { await updateDriverLinecheck(item.id, 'STARTLINEREAL') }} />
+          <Text>Serviço: {item.service}</Text>
+          <Text>Logistica: {item.logistic}</Text>
+          <Text>Fábrica: {item.manufacture}</Text>
+          <Text>Veículo: {item.vehicle}</Text>
+          <Text>Fim jornada: {item.endLineModel}</Text>
+          <Button title='Check end journey' onPress={async () => { await updateDriverLinecheck(item.id, 'ENDLINEREAL') }} />
         </View>
       ))}
-    </View>
+    </ScrollView>
   )
 }
