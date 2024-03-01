2e277874cafb343844cf612b3df878aa9b1573fa
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-01 17:25:25 -0300

    feat: implement multi select to days of the week

--- src/presentation/pages/manager/create.line.form.tsx
+++ src/presentation/pages/manager/create.line.form.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { type ParamListBase, type RouteProp } from '@react-navigation/native'
 import { Text, View } from 'react-native'
 import { type EntityNames } from '../../../domain/entities/entity.names'
 import SelectPicker from 'react-native-picker-select'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 export interface ParamList extends ParamListBase {
   'CREATELINE': { data: EntityNames[] }
@@ -14,6 +15,9 @@ interface Props {
 
 export default function CreateLineForm ({ route }: Props): React.JSX.Element {
   const { data } = route.params
+  const [selectedDays, setSelectedDays] = useState([])
+  const [open, setOpen] = useState(false)
+
   return (
     <View>
       <Text>Selecione o motorista</Text>
@@ -50,6 +54,35 @@ export default function CreateLineForm ({ route }: Props): React.JSX.Element {
         items={data.filter(item => item.origin === 'vehicles').map(item => ({
           label: item.name, value: item.name
         }))}
+      />
+      <Text>Selecione os dias da semana</Text>
+      <DropDownPicker
+        open={open}
+        setOpen={setOpen}
+        value={selectedDays}
+        setValue={setSelectedDays}
+        multiple
+        placeholder='Selecione'
+        multipleText={
+          selectedDays.length > 1
+            ? `${selectedDays.length} dias selecionados`
+            : `${selectedDays.length} dia selecionado`
+        }
+        items={[{
+          label: 'Domingo', value: 'Sunday'
+        }, {
+          label: 'Segunda', value: 'Monday'
+        }, {
+          label: 'Terça', value: 'Tuesday'
+        }, {
+          label: 'Quarta', value: 'Wednesday'
+        }, {
+          label: 'Quinta', value: 'Thursday'
+        }, {
+          label: 'Sexta', value: 'Friday'
+        }, {
+          label: 'Sábado', value: 'Saturday'
+        }]}
       />
     </View>
   )
