757bbe4a678a748ed278f96fab2cb0c1d4b3259d
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-01 17:52:52 -0300

    feat: add react hook form in fields, except multi select

--- src/presentation/pages/manager/create.line.form.tsx
+++ src/presentation/pages/manager/create.line.form.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { type ParamListBase, type RouteProp } from '@react-navigation/native'
-import { Text, View } from 'react-native'
+import { Button, ScrollView, Text, TextInput } from 'react-native'
 import { type EntityNames } from '../../../domain/entities/entity.names'
 import SelectPicker from 'react-native-picker-select'
 import DropDownPicker from 'react-native-dropdown-picker'
+import { type SubmitHandler, useForm } from 'react-hook-form'
 
 export interface ParamList extends ParamListBase {
   'CREATELINE': { data: EntityNames[] }
@@ -13,44 +14,71 @@ interface Props {
   route: RouteProp<ParamList, 'CREATELINE'>
 }
 
+interface Inputs {
+  account: string
+  logistic: string
+  service: string
+  manufacture: string
+  vehicle: string
+  startJourney: string
+  startLine: string
+  endLine: string
+}
+
 export default function CreateLineForm ({ route }: Props): React.JSX.Element {
   const { data } = route.params
+  const { register, setValue, handleSubmit } = useForm<Inputs>()
   const [selectedDays, setSelectedDays] = useState([])
   const [open, setOpen] = useState(false)
 
+  useEffect(() => {
+    register('account')
+    register('logistic')
+    register('service')
+    register('manufacture')
+    register('vehicle')
+    register('startJourney')
+    register('startLine')
+    register('endLine')
+  }, [register])
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+  }
+
   return (
-    <View>
+    <ScrollView>
       <Text>Selecione o motorista</Text>
       <SelectPicker
-        onValueChange={() => {}}
+        onValueChange={(value: string) => { setValue('account', value) }}
         items={data.filter(item => item.origin === 'accounts').map(item => ({
           label: item.name, value: item.name
         }))}
       />
       <Text>Selecione o trajeto</Text>
       <SelectPicker
-        onValueChange={() => {}}
+        onValueChange={(value: string) => { setValue('logistic', value) }}
         items={data.filter(item => item.origin === 'logistics').map(item => ({
           label: item.name, value: item.name
         }))}
       />
       <Text>Selecione o serviço</Text>
       <SelectPicker
-        onValueChange={() => {}}
+        onValueChange={(value: string) => { setValue('service', value) }}
         items={data.filter(item => item.origin === 'services').map(item => ({
           label: item.name, value: item.name
         }))}
       />
       <Text>Selecione a fábrica de destino</Text>
       <SelectPicker
-        onValueChange={() => {}}
+        onValueChange={(value: string) => { setValue('manufacture', value) }}
         items={data.filter(item => item.origin === 'manufactures').map(item => ({
           label: item.name, value: item.name
         }))}
       />
       <Text>Selecione o veiculo</Text>
       <SelectPicker
-        onValueChange={() => {}}
+        onValueChange={(value: string) => { setValue('vehicle', value) }}
         items={data.filter(item => item.origin === 'vehicles').map(item => ({
           label: item.name, value: item.name
         }))}
@@ -84,6 +112,22 @@ export default function CreateLineForm ({ route }: Props): React.JSX.Element {
           label: 'Sábado', value: 'Saturday'
         }]}
       />
-    </View>
+      <Text>Horário de inicio da jornada</Text>
+      <TextInput
+        placeholder='inicio jornada'
+        onChangeText={(text) => { setValue('startJourney', text) }}
+      />
+      <Text>Horário de inicio da linha</Text>
+      <TextInput
+        placeholder='inicio linha'
+        onChangeText={(text) => { setValue('startLine', text) }}
+      />
+      <Text>Horário de fim da jornada</Text>
+      <TextInput
+        placeholder='fim jornada'
+        onChangeText={(text) => { setValue('endLine', text) }}
+      />
+      <Button title='Criar' onPress={handleSubmit(onSubmit)}/>
+    </ScrollView>
   )
 }
