9ac8bbd02893c236ac1d31737b900dc590bd66b6
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-02 15:08:43 -0300

    feat: add create line factory

---
+++ src/core/factories/data/work/create.line.factory.ts
@@ -0,0 +1,9 @@
+import { type ICreateLine } from '../../../../data/protocols/usecases/create.line.protocol'
+import { CreateLine } from '../../../../data/usecases/create.line'
+import { makeHttpClient } from '../../infra/adapters/http.client.factory'
+
+export const makeCreateLine = (): ICreateLine => {
+  const url = 'http://10.0.2.2:8080/api/checkpoint/line'
+  const httpClient = makeHttpClient()
+  return new CreateLine(url, httpClient)
+}
