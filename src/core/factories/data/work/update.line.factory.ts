b967b3ccdfdbf8e7d5cf3ccd370915bbac5c30b8
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-07 18:59:09 -0300

    feat: create update line factory

---
+++ src/core/factories/data/work/update.line.factory.ts
@@ -0,0 +1,9 @@
+import { type IUpdateLine } from '../../../../data/protocols/usecases/update.line.protocol'
+import { UpdateLine } from '../../../../data/usecases/update.line'
+import { makeHttpClient } from '../../infra/adapters/http.client.factory'
+
+export const makeUpdateLine = (): IUpdateLine => {
+  const url = 'http://10.0.2.2:8080/api/checkpoint/line'
+  const httpPutClient = makeHttpClient()
+  return new UpdateLine(url, httpPutClient)
+}
