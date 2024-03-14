8b8722369faeeae4a4a94fd8c59e8c17a62ec059
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-14 15:54:46 -0300

    fix: remove notification service

--- src/infra/adapters/notification.adapter.ts
+++ src/infra/adapters/notification.adapter.ts
@@ -1,28 +1,7 @@
-import * as ExpoNotification from 'expo-notifications'
 import { type INotification } from '../protocols/notification.protocol'
-import { NotPermittedNotification } from '../../core/exceptions/not.permitted.notification.error'
-
-ExpoNotification.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-    shouldShowAlert: true
-  })
-})
 
 export class Notification implements INotification {
-  constructor (
-    private readonly id: string
-  ) {}
-
   public async getToken (): Promise<string> {
-    await this.getPermission()
-    const token = (await ExpoNotification.getExpoPushTokenAsync({ projectId: this.id })).data
-    return token
-  }
-
-  private async getPermission (): Promise<void> {
-    const { status } = await ExpoNotification.getPermissionsAsync()
-    if (status !== 'granted') throw new NotPermittedNotification()
+    return 'implements get device token'
   }
 }
