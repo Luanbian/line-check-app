e91f2506183f2df68140f5e07a748c2d54797dce
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-13 21:37:37 -0300

    feat: send notification through axios API

--- src/core/factories/infra/adapters/notification.factory.ts
+++ src/core/factories/infra/adapters/notification.factory.ts
@@ -1,7 +1,10 @@
 import { Notification } from '../../../../infra/adapters/notification.adapter'
 import { type INotification } from '../../../../infra/protocols/notification.protocol'
+import { makeHttpClient } from './http.client.factory'
 
 export const makeNotification = (): INotification => {
   const id = 'da6ccbef-e512-453e-9eff-78cc27524584'
-  return new Notification(id)
+  const url = 'https://exp.host/--/api/v2/push/send'
+  const httpPostClient = makeHttpClient()
+  return new Notification(id, url, httpPostClient)
 }
--- src/infra/adapters/notification.adapter.ts
+++ src/infra/adapters/notification.adapter.ts
@@ -1,5 +1,12 @@
 import * as ExpoNotification from 'expo-notifications'
 import { type INotification } from '../protocols/notification.protocol'
+import { type IHttpClient } from '../protocols/http.post.client.protocol'
+
+interface message {
+  to: string
+  title: string
+  body: string
+}
 
 ExpoNotification.setNotificationHandler({
   handleNotification: async () => ({
@@ -10,21 +17,35 @@ ExpoNotification.setNotificationHandler({
 })
 
 export class Notification implements INotification {
-  constructor (private readonly id: string) {}
+  constructor (
+    private readonly id: string,
+    private readonly url: string,
+    private readonly httpPostClient: IHttpClient
+  ) {}
 
   public async notify (title: string, body: string): Promise<void> {
     const { status } = await ExpoNotification.getPermissionsAsync()
     if (status !== 'granted') return
     const token = await this.getToken()
     console.log(token)
-    await ExpoNotification.scheduleNotificationAsync({
-      content: { title, body },
-      trigger: null,
-      identifier: token
+    const message = this.mountMessage(token, title, body)
+    await this.httpPostClient.request({
+      method: 'POST',
+      url: this.url,
+      body: message,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
     })
   }
 
   private async getToken (): Promise<string> {
     return (await ExpoNotification.getExpoPushTokenAsync({ projectId: this.id })).data
+  }
+
+  private mountMessage (token: string, title: string, body: string): message {
+    const message: message = { to: 'token', title, body }
+    return message
   }
 }
